603575dfbbca9abe28bd3f88a0b30e76b45635df
SchoeniPfusch <soesic18@htl-kaindorf.at>
2023-03-14 10:51:29 +0100

    idk

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class CameraController : MonoBehaviour
     private float maxZoom;
 
     private Vector3 dragOrigin;
+    private int UIlayer;
+    private bool uiHover;
 
 
     // Start is called before the first frame update
@@ -21,40 +24,70 @@ public class CameraController : MonoBehaviour
         camera = Camera.main;
         StartSize = camera.orthographicSize;
 
-       
+        UIlayer = LayerMask.NameToLayer("UI");
     }
 
     // Update is called once per frame
     void Update()
     {
+        uiHover = IsPointerOverUIElement();
         HandleScrolling();
         HandleMouseDrag();
     }
 
     private void HandleScrolling()
     {
-        if ((camera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed < StartSize) && (camera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed > maxZoom))
+        if (!uiHover)
         {
-            camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+            if ((camera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed < StartSize) && (camera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed > maxZoom))
+            {
+                camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+            }
+            else if (camera.orthographicSize < StartSize / 2)
+            {
+                camera.orthographicSize = maxZoom + 1;
+            }
         }
-        else if (camera.orthographicSize < StartSize / 2)
-        {
-            camera.orthographicSize = maxZoom + 1;
-        }
+    
     }
 
     private void HandleMouseDrag()
     {
-        if (Input.GetMouseButtonDown(2))
-            dragOrigin = camera.ScreenToWorldPoint(Input.mousePosition);
-       
+            if (Input.GetMouseButtonDown(2))
+                dragOrigin = camera.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButton(2))
+
+            if (Input.GetMouseButton(2))
+            {
+                Vector3 difference = dragOrigin - camera.ScreenToWorldPoint(Input.mousePosition);
+
+                camera.transform.position += difference;
+            }
+    }
+
+    public bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
         {
-            Vector3 difference = dragOrigin - camera.ScreenToWorldPoint(Input.mousePosition);
-
-            camera.transform.position += difference;
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UIlayer)
+                return true;
         }
+        return false;
+    }
+
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
     }
 
 
