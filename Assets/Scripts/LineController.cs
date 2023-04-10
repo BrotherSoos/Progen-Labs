d8e06fcb473cf64d020427ac10fa65312c99754e
SchoeniPfusch <soesic18@htl-kaindorf.at>
2023-04-10 19:19:26 +0200

    lines

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LineController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LineRenderer lr;
+    private List<Transform> points = new List<Transform>();
+    private bool drawing;
+
+    private void Awake()
     {
+        lr = GetComponent<LineRenderer>();
+        drawing = false;
+    }
+
+    private void Update()
+    {
+        Vector2 screenPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+        if (drawing && Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(new GameObject(), new Vector3(worldPos.x, worldPos.y, 0), Quaternion.identity);
+            points.Add(go.transform);
+            SetUpLines();
+        }
+        if(points.Count >= 1)
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                lr.SetPosition(i, points[i].position);
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetUpLines()
     {
-        
+        lr.positionCount = points.Count;
+
+    }
+
+    public void drawEnabled()
+    {
+        drawing = true;
     }
 }
